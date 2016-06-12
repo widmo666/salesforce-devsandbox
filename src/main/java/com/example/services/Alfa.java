85d13c5b7415f77b86f92c66027bf4a73dcd990c
Michał <widmo666@o2.pl>
2016-06-12 21:34:53 +0200

    Third webservice

--- src/main/java/com/example/services/Alfa.java
+++ src/main/java/com/example/services/Alfa.java
@@ -1,10 +1,15 @@
 package com.example.services;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 @Path("/alfa")
+@Produces(MediaType.APPLICATION_JSON)
 public class Alfa {
 
+	@GET
 	@Path("/test")
 	public String getWebserviceTest(){
 		return "Alfa Webservice OK!";
